089659f80ae24b2f774b3697b56c5f2476691ccc
Moises Santos de Melo Freitas <73970290+demelodev@users.noreply.github.com>
2021-03-10 08:41:51 -0300

    atualização de pastas
    Upload de pastas.

---
+++ index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+ 
+    <title>Projeto Dev Web</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+
+  </head>
+  <body>
+
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Danki.code</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li class="active"><a href="">Home</a></li>
+            <li><a href="sobre">Sobre</a></li>
+            <li><a href="contato">Contato</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+    <div class="box">
+      <section class="banner">
+        <div class="overlay"></div>
+        <div class="container chamada-banner">
+          <div class="row">
+            <div class="col-md-12 text-center">
+              <h2 style="color: white;">Danki.Code</h2>
+              <p>Empresa voltada para desenvolvimento web e marketing digital</p>
+              <a href="">Saiba mais!</a>
+            </div><!-- col-md-12 -->       
+          </div><!-- row -->   
+        </div>    
+      </section>
+
+      <section class="cadastro-lead">
+      <div class="container">
+        <div class="row text-center">
+          <div class="col-md-6">
+            <h2><span class="glyphicon glyphicon-star" aria-hidden="true"></span> Entre na nossa lista!</h2>
+          </div>
+          <div class="col-md-6">
+            <form method="post">
+              <input type="text" name="nome" /><input type="submit" value="Enviar" />
+            </form>
+          </div>          
+        </div>
+      </div>
+      
+    </section>
+    <section class="depoimento text-center">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <h2>Depoimento</h2>
+            <blockquote >
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in urna at libero dignissim tincidunt. Aliquam magna neque, blandit ut odio id, facilisis varius nunc. Curabitur ac nisi ut purus semper finibus. Aliquam erat volutpat. Maecenas sed est porta elit euismod facilisis ut eu tortor. Ut dapibus neque nec semper pretium. Etiam iaculis, lorem quis placerat lobortis, dolor nibh vehicula felis, feugiat pellentesque ante leo id quam.</p>
+            </blockquote>
+          </div>
+        </div>
+      </div>
+      
+    </section>
+    <section class="diferenciais text-center">
+      <h2>Conheça nossa empresa!</h2>
+      <div class="container diferenciais-container">
+        <div class="row">
+          <div class="col-md-4">
+            <h3><span class="glyphicon glyphicon-glass"></span></h3>
+            <h2>Diferencial #1</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in urna at libero dignissim tincidunt. Aliquam magna neque, blandit ut odio id, facilisis varius nunc. Curabitur ac nisi ut purus semper finibus. Aliquam erat volutpat. Maecenas sed est porta elit euismod facilisis ut eu tortor. Ut dapibus neque nec semper pretium. Etiam iaculis, lorem quis placerat lobortis, dolor nibh vehicula felis, feugiat pellentesque ante leo id quam.</p>
+          </div>
+          <div class="col-md-4">
+            <h3><span class="glyphicon glyphicon-star"></span></h3>
+            <h2>Diferencial #1</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in urna at libero dignissim tincidunt. Aliquam magna neque, blandit ut odio id, facilisis varius nunc. Curabitur ac nisi ut purus semper finibus. Aliquam erat volutpat. Maecenas sed est porta elit euismod facilisis ut eu tortor. Ut dapibus neque nec semper pretium. Etiam iaculis, lorem quis placerat lobortis, dolor nibh vehicula felis, feugiat pellentesque ante leo id quam.</p>
+          </div>
+          <div class="col-md-4">
+            <h3><span class="glyphicon glyphicon-heart"></span></h3>
+            <h2>Diferencial #1</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in urna at libero dignissim tincidunt. Aliquam magna neque, blandit ut odio id, facilisis varius nunc. Curabitur ac nisi ut purus semper finibus. Aliquam erat volutpat. Maecenas sed est porta elit euismod facilisis ut eu tortor. Ut dapibus neque nec semper pretium. Etiam iaculis, lorem quis placerat lobortis, dolor nibh vehicula felis, feugiat pellentesque ante leo id quam.</p>
+          </div>                   
+        </div>
+      </div>
+    </section>
+    </div><!-- box -->
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
