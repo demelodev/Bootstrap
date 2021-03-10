6a84d25769cccea6fe5cf0b4ced2058b4039eaa5
Moises Santos de Melo Freitas <73970290+demelodev@users.noreply.github.com>
2021-03-10 13:51:18 -0300

    updated index.php
    uptdate

--- index.php
+++ index.php
@@ -4,16 +4,13 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
- 
     <title>Projeto Dev Web</title>
-
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/style.css">
-
+    <link href="css/style.css" rel="stylesheet">
   </head>
   <body>
-
+   
     <nav class="navbar navbar-default navbar-fixed-top">
       <div class="container">
         <div class="navbar-header">
@@ -23,7 +20,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Danki.code</a>
+          <a class="navbar-brand" href="#">Danki Code</a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav navbar-right">
@@ -34,73 +31,74 @@
         </div><!--/.nav-collapse -->
       </div>
     </nav>
-
     <div class="box">
       <section class="banner">
         <div class="overlay"></div>
         <div class="container chamada-banner">
           <div class="row">
-            <div class="col-md-12 text-center">
-              <h2 style="color: white;">Danki.Code</h2>
-              <p>Empresa voltada para desenvolvimento web e marketing digital</p>
-              <a href="">Saiba mais!</a>
-            </div><!-- col-md-12 -->       
-          </div><!-- row -->   
-        </div>    
+              <div class="col-md-12 text-center">
+                  <h2><?php echo htmlentities('<'); ?>Danki.Code<?php echo htmlentities('>');  ?></h2>
+                  <p>Empresa voltada para desenvolvimento web e marketing digital</p>
+                  <a href="">Saiba Mais!</a>
+              </div><!--col-md-12-->
+          </div><!--row-->
+        </div>
+      </section>
+      <section class="cadastro-lead">
+          <div class="container">
+            <div class="row text-center">
+                <div class="col-md-6">
+                  <h2><span class="glyphicon glyphicon-star" aria-hidden="true"></span> Entre na nossa lista!</h2>
+                </div>
+                <div class="col-md-6">
+                  <form method="post">
+                    <input type="text" name="nome" /><input type="submit" value="Enviar" />
+                  </form>
+                </div>
+            </div>
+          </div>
       </section>
 
-      <section class="cadastro-lead">
-      <div class="container">
-        <div class="row text-center">
-          <div class="col-md-6">
-            <h2><span class="glyphicon glyphicon-star" aria-hidden="true"></span> Entre na nossa lista!</h2>
-          </div>
-          <div class="col-md-6">
-            <form method="post">
-              <input type="text" name="nome" /><input type="submit" value="Enviar" />
-            </form>
-          </div>          
+      <section class="depoimento text-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                  <h2>Depoimento</h2>
+                    <blockquote>
+                      <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eget lorem varius, pellentesque ipsum convallis, suscipit neque. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam quis orci quam. Phasellus dictum erat at nibh bibendum, eget porta urna pretium. Maecenas vel augue massa. Nulla facilisi. Nulla a suscipit quam, eu pharetra justo."</p>
+                  </blockquote>
+                </div>
+            </div>
         </div>
-      </div>
-      
-    </section>
-    <section class="depoimento text-center">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <h2>Depoimento</h2>
-            <blockquote >
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in urna at libero dignissim tincidunt. Aliquam magna neque, blandit ut odio id, facilisis varius nunc. Curabitur ac nisi ut purus semper finibus. Aliquam erat volutpat. Maecenas sed est porta elit euismod facilisis ut eu tortor. Ut dapibus neque nec semper pretium. Etiam iaculis, lorem quis placerat lobortis, dolor nibh vehicula felis, feugiat pellentesque ante leo id quam.</p>
-            </blockquote>
-          </div>
-        </div>
-      </div>
-      
-    </section>
-    <section class="diferenciais text-center">
-      <h2>Conheça nossa empresa!</h2>
-      <div class="container diferenciais-container">
-        <div class="row">
-          <div class="col-md-4">
-            <h3><span class="glyphicon glyphicon-glass"></span></h3>
-            <h2>Diferencial #1</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in urna at libero dignissim tincidunt. Aliquam magna neque, blandit ut odio id, facilisis varius nunc. Curabitur ac nisi ut purus semper finibus. Aliquam erat volutpat. Maecenas sed est porta elit euismod facilisis ut eu tortor. Ut dapibus neque nec semper pretium. Etiam iaculis, lorem quis placerat lobortis, dolor nibh vehicula felis, feugiat pellentesque ante leo id quam.</p>
-          </div>
-          <div class="col-md-4">
-            <h3><span class="glyphicon glyphicon-star"></span></h3>
-            <h2>Diferencial #1</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in urna at libero dignissim tincidunt. Aliquam magna neque, blandit ut odio id, facilisis varius nunc. Curabitur ac nisi ut purus semper finibus. Aliquam erat volutpat. Maecenas sed est porta elit euismod facilisis ut eu tortor. Ut dapibus neque nec semper pretium. Etiam iaculis, lorem quis placerat lobortis, dolor nibh vehicula felis, feugiat pellentesque ante leo id quam.</p>
-          </div>
-          <div class="col-md-4">
-            <h3><span class="glyphicon glyphicon-heart"></span></h3>
-            <h2>Diferencial #1</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in urna at libero dignissim tincidunt. Aliquam magna neque, blandit ut odio id, facilisis varius nunc. Curabitur ac nisi ut purus semper finibus. Aliquam erat volutpat. Maecenas sed est porta elit euismod facilisis ut eu tortor. Ut dapibus neque nec semper pretium. Etiam iaculis, lorem quis placerat lobortis, dolor nibh vehicula felis, feugiat pellentesque ante leo id quam.</p>
-          </div>                   
-        </div>
-      </div>
-    </section>
+      </section>
 
-    <section class="equipe">
+    <section class="diferenciais text-center">
+      <h2>Conheça nossa empresa</h2>
+        <div class="container diferenciais-container">
+            <div class="row">
+              <div class="col-md-4">
+                  <h3><span class="glyphicon glyphicon-glass"></span></h3>
+                  <h2>Diferencial #1</h2>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eget lorem varius, pellentesque ipsum convallis, suscipit neque. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam quis orci quam. Phasellus dictum erat at nibh bibendum, eget porta urna pretium. Maecenas vel augue massa. Nulla facilisi. Nulla a suscipit quam, eu pharetra justo.</p>
+              </div>
+
+              <div class="col-md-4">
+                  <h3><span class="glyphicon glyphicon-star"></span></h3>
+                  <h2>Diferencial #1</h2>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eget lorem varius, pellentesque ipsum convallis, suscipit neque. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam quis orci quam. Phasellus dictum erat at nibh bibendum, eget porta urna pretium. Maecenas vel augue massa. Nulla facilisi. Nulla a suscipit quam, eu pharetra justo.</p>
+              </div>
+
+              <div class="col-md-4">
+                  <h3><span class="glyphicon glyphicon-heart"></span></h3>
+                  <h2>Diferencial #1</h2>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eget lorem varius, pellentesque ipsum convallis, suscipit neque. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam quis orci quam. Phasellus dictum erat at nibh bibendum, eget porta urna pretium. Maecenas vel augue massa. Nulla facilisi. Nulla a suscipit quam, eu pharetra justo.</p>
+              </div>
+
+            </div>
+        </div>
+      </section>
+
+      <section class="equipe">
         <h2>Equipe</h2>
         <div class="container equipe-container">
             <div class="row">
@@ -170,7 +168,74 @@
         </div><!--equipe-container-->
       </section>
 
-    </div><!-- box -->
+
+      <section class="final-site">
+          <div class="container">
+              <div class="row">
+
+                  <div class="col-md-6">
+                    <h2>Fale conosco</h2>
+                    <form>
+                        <div class="form-group">
+                          <label for="email">Nome:</label>
+                          <input type="text" name="nome" class="form-control" id="nome">
+                        </div>
+
+                         <div class="form-group">
+                          <label for="email">E-mail:</label>
+                          <input type="email" name="email" class="form-control" id="email">
+                        </div>
+
+                         <div class="form-group">
+                          <label for="email">Mensagem:</label>
+                          <textarea class="form-control"></textarea>
+                        </div>
+                      
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                  </div>
+
+                  <div class="col-md-6">
+                      <h2>Nossos planos</h2>
+                        <table class="table">
+                            <thead>
+                              <tr>
+                                <th>Plano Semanal</th>
+                                <th>Plano Diário</th>
+                                <th>Plano Anual</th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                              <tr>
+                                <td>R$199,00</td>
+                               <td>R$299,00</td>
+                                <td>R$399,00</td>
+                              </tr>
+
+                              <tr>
+                                <td>R$199,00</td>
+                               <td>R$299,00</td>
+                                <td>R$399,00</td>
+                              </tr>
+
+                               <tr>
+                                <td>R$199,00</td>
+                               <td>R$299,00</td>
+                                <td>R$399,00</td>
+                              </tr>
+                            </tbody>
+                          </table>
+                  </div>
+
+              </div>
+          </div>
+      </section>
+
+      <footer>
+        <p class="text-center">Todos os direitos reservados!</p>
+      </footer>
+
+    </div><!--box-->
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
